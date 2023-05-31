a5a9032790f19d3a20faf76c620f4ac19aee9016
jecklzh <578360521@qq.com>
2023-05-31 21:47:51 +0800

    Site updated: 2023-05-31 21:47:50

--- static/hingle.js
+++ static/hingle.js
@@ -164,14 +164,14 @@ var Paul_Hingle = function (config) {
     // ! Hexo 特别功能
     //
 
-    // Hexo 百度搜索
+    // Hexo 谷歌搜索
     this.hexo_search = function () {
         var form = ks.select(".head-search"), input = ks.select(".head-search input");
 
         form.onsubmit = function (ev) {
             ev.preventDefault();
 
-            window.open("https://www.baidu.com/s?wd=site:" + location.host + " " + input.value.trim());
+            window.open("https://www.google.com/search?q=site:" + location.host + " " + input.value.trim());
         }        
     }
 
